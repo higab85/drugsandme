7af2bf7ea5d097b8a741a852f7d0fd1d47c7bfe9
Gab <gabh@protonmail.com>
2018-10-20 13:47:43 +0200

    removed logging

--- manage.py
+++ manage.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import logging
-
-logging.basicConfig(filename='dev.log', filemode='w', level=logging.DEBUG, format='[%(asctime)s]%(levelname)s: %(message)s', datefmt='%H:%M:%S')
-
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drugsandme.settings.dev")
